f210859a53dc8fab75ec699000c9f4e81d9820dc
Intuitive Harmony <j.horst77@gmail.com>
2023-05-12 08:28:03 -0600

    dates update

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -1,18 +1,16 @@
-
-
 function DatesList() {
   return (
     <>
-        <h2 className="datesHeader blueShadow">Upcoming Open Circles:</h2>
-        <ul className="dates calendarCheck">
-            <li>April 24, 2023</li>
-            <li>May 29, 2023</li>
-            <li>July 3, 2023</li>
-            <li>August 7, 2023</li>
-            <li>September 4, 2023</li>
-            <li>October 9, 2023</li>
-        </ul>
+      <h2 className="datesHeader blueShadow">Upcoming Open Circles:</h2>
+      <ul className="dates calendarCheck">
+        <li>April 24, 2023</li>
+        <li>June 5, 2023</li>
+        <li>July 10, 2023</li>
+        <li>August 7, 2023</li>
+        <li>September 4, 2023</li>
+        <li>October 9, 2023</li>
+      </ul>
     </>
-  )
+  );
 }
-export default DatesList
+export default DatesList;
