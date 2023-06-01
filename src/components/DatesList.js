402fde19030aeb7938a9e3d08d837096004ba10f
Intuitive Harmony <j.horst77@gmail.com>
2023-06-01 17:51:29 -0600

    new dates

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -6,8 +6,8 @@ function DatesList() {
         <li>June 5, 2023</li>
         <li>July 10, 2023</li>
         <li>August 7, 2023</li>
-        <li>September 4, 2023</li>
-        <li>October 9, 2023</li>
+        <li>September 11, 2023</li>
+        <li>October 16, 2023</li>
       </ul>
     </>
   );
