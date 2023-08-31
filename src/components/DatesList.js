a7662ba20478e6dd0996fefc001d8b110996f6b4
Intuitive Harmony <j.horst77@gmail.com>
2023-08-31 14:34:11 -0600

    2024 dates

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -3,11 +3,16 @@ function DatesList() {
     <>
       <h2 className="datesHeader blueShadow">Upcoming Open Circles:</h2>
       <ul className="dates calendarCheck">
-        <li>June 5, 2023</li>
-        <li>July 10, 2023</li>
-        <li>August 7, 2023</li>
         <li>September 11, 2023</li>
         <li>October 16, 2023</li>
+        <li>February 5, 2024</li>
+        <li>March 11, 2024</li>
+        <li>April 15, 2024</li>
+        <li>May 20, 2024</li>
+        <li>June 24, 2024</li>
+        <li>July 29, 2024</li>
+        <li>September 9, 2024</li>
+        <li>September 30, 2024</li>
       </ul>
     </>
   );
