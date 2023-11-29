a12b2ac535b389b508960ac362dfa2ea25f6cab6
Intuitive Harmony <j.horst77@gmail.com>
2023-11-29 09:47:29 -0700

    sept update

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -3,7 +3,23 @@ function DatesList() {
     <>
       <h2 className="datesHeader blueShadow">Upcoming Open Circles:</h2>
       <ul className="dates calendarCheck">
-        <li>September 11, 2023</li>
+        <li>
+          <a
+            href="data:text/calendar;charset=utf8,BEGIN:VCALENDAR
+VERSION:2.0
+BEGIN:VEVENT
+SUMMARY:Event Title
+DESCRIPTION:Event Description
+LOCATION:Event Location
+DTSTART:20231001T120000
+DTEND:20231001T140000
+END:VEVENT
+END:VCALENDAR"
+            download="event.ics"
+          >
+            September 11, 2023
+          </a>
+        </li>
         <li>October 16, 2023</li>
         <li>February 5, 2024</li>
         <li>March 11, 2024</li>
