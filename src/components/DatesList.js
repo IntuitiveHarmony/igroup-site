b39682b0c5b7fe17bf69f631ed58e39e1d230ac1
Kevin Wysocki <kevinmwysocki@gmail.com>
2024-07-15 13:10:10 -0600

    Removing expired meeting dates

--- src/components/DatesList.js
+++ src/components/DatesList.js
@@ -3,8 +3,6 @@ function DatesList() {
     <>
       <h2 className="datesHeader blueShadow">Upcoming Open Circles:</h2>
       <ul className="dates calendarCheck">
-        <li>May 6, 2024</li>
-        <li>June 24, 2024</li>
         <li>August 19, 2024</li>
         <li>October 7, 2024</li>
       </ul>
